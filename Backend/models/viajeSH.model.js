d02e3a686ccbddec4d618ccbdc4906a68ea924be
Javiera <75714888+JBManzo@users.noreply.github.com>
2022-10-10 20:10:20 -0300

    Add files via upload

---
+++ Backend/models/viajeSH.model.js
@@ -0,0 +1,39 @@
+import { Schema, model } from "mongoose";
+
+const schema = new Schema({
+    
+    ID_SH:{
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    rut:{
+        type: String, 
+        required: true
+    },
+    dire_inicio:{
+        type: String,
+        required: true
+    },
+    dire_destino: {
+        type:String,
+        required: true
+    },
+    hora: {
+        type:String,
+        required: true
+    },
+    fecha:{
+        type: String,
+        required: true
+    },
+    metodo:{
+        Type: String,
+
+
+    }
+})
+export const ViajeSH = model("ViajesSH", schema);
