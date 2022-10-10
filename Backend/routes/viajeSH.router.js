506d470e3d6788ddc8953fe20a1731fb1a8892e2
Javiera <75714888+JBManzo@users.noreply.github.com>
2022-10-10 20:13:23 -0300

    Update viajeSH.router.js

--- Backend/routes/viajeSH.router.js
+++ Backend/routes/viajeSH.router.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
 import { loginRequired } from "../controllers/auth.controller.js";
-import { ViajeSH} from "../controllers/viajeSH.controller";
+import { SolicitudViajeSH} from "../controllers/viajeSH.controller";
 
 const router = Router();
-router.use("/SolicitudViajeSH", loginRequired, ViajeSH);
+router.use("/SolicitudViajeSH", loginRequired, SolicitudViajeSH);
 router.use("/VerViajeSH", loginRequired, VerViajeSH);
 router.use("/EditarViajeSH", loginRequired, EditarViajeSH);
 router.use("/ElimitarViajeSH", loginRequired, EliminarViajeSH);
